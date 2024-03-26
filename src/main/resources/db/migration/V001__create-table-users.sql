a34c33038d1de160738f368aa37c707a6582f2c8
Valeria <santos.valeria1501@gmail.com>
2024-03-26 14:33:15 -0300

    fix(database): changing the data type

--- src/main/resources/db/migration/V001__create-table-users.sql
+++ src/main/resources/db/migration/V001__create-table-users.sql
@@ -1,12 +1,11 @@
 create table users(
-UserIdInternal bigint not null auto_increment primary key,
-UserIdExternal varchar(36) default (UUID()) not null unique,
-Name varchar(255) not null,
-Username varchar(20) not null unique,
-Email varchar(255) not null unique,
-Password  varchar(255) not null,
-Role enum('STUDENT', 'INSTRUCTOR', 'ADMINISTRATOR') not null,
-CreationDate timestamp default current_timestamp,
+id bigint primary key auto_increment,
+name varchar(255) not null,
+username varchar(20) not null unique,
+email varchar(255) not null unique,
+password  varchar(255) not null,
+role enum('STUDENT', 'INSTRUCTOR', 'ADMIN') not null,
+creation_date datetime not null,
 CONSTRAINT chk_username CHECK (Username = LOWER(Username) AND Username NOT LIKE '%[0-9]%' AND Username NOT LIKE '% %'),
 CONSTRAINT chk_email CHECK (Email LIKE '%_@__%.__%')
 )
