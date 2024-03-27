05a89105d9a936f67026f34ef1141d96a7169234
Valeria <santos.valeria1501@gmail.com>
2024-03-27 20:54:50 -0300

    style: code formatting

--- src/main/java/com/github/project/study/api/entities/Course.java
+++ src/main/java/com/github/project/study/api/entities/Course.java
@@ -16,21 +16,28 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
+
     @Size(max = 255)
     @NotBlank(message = "The field name is mandatory")
     private String name;
+
     @NotBlank(message = "The field code is mandatory")
     @Column(unique = true)
     @Size(max = 10)
     private String code;
+
     @NotBlank(message = "The field instructor is mandatory")
     @Size(max = 255)
     private String instructor;
+
     private String description;
+
     @Enumerated(EnumType.STRING)
     private Status status;
+
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
+
     @Column(name = "inactivation_date")
     private LocalDateTime inactivationDate;
 }
--- src/main/java/com/github/project/study/api/entities/User.java
+++ src/main/java/com/github/project/study/api/entities/User.java
@@ -19,24 +19,30 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
+
     @NotBlank(message = "The field name is mandatory")
     @Size(max = 255)
     private String name;
+
     @NotBlank(message = "The field username is mandatory")
     @Pattern(regexp = "^[a-z]+$", message = "The username must contain only lowercase characters")
     @Column(unique = true)
     @Size(max = 20)
     private String username;
+
     @NotBlank(message = "\n" + "The field email is mandatory")
     @Email(message = "The email must be in a valid format")
     @Column(unique = true)
     @Size(max = 255)
     private String email;
+
     @NotBlank(message = "The field password is mandatory")
     @Size(max = 255)
     private String password;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
 
